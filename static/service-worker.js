f4ec57667e8a5c44da24b9d9a9e78d23484b132d
RonaldoGPP <104438683+RonaldoGPP@users.noreply.github.com>
2023-06-23 04:01:18 +0700

    Add files via upload
    bug fix

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,4 +1,5 @@
 const cacheName = 'flask-PWA-v1';
+const staticcache = 'css-cache'
 const filesToCache = [
     '/',
     '/static/app.js',
@@ -6,7 +7,11 @@ const filesToCache = [
     '/upload/',
     '/gallery/',
     'http://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css',
-    'http://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js'
+'http://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js'
+    
+];
+const staticfiles = ['http://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css',
+'http://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js'
 ];
 
 self.addEventListener('install', (e) => {
@@ -15,6 +20,9 @@ self.addEventListener('install', (e) => {
         const cache = await caches.open(cacheName);
         console.log('[ServiceWorker] Caching app shell');
         await cache.addAll(filesToCache);
+        const cache2 = await caches.open(staticcache);
+        console.log('[ServiceWorker] Caching app shell');
+        await cache2.addAll(staticfiles);
     })());
 });
 
@@ -28,11 +36,26 @@ self.addEventListener('activate', (e) => {
                 await caches.delete(key);
             }
         });
+        const cache = await caches.open(cacheName);
+        console.log('[ServiceWorker] Updating cache');
+        await cache.addAll(filesToCache);
     })());
     return self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
+    if (e.request.method === 'POST'&& e.request.url.includes('/upload')) {
+        // Exclude handling redirects for POST requests
+        return;
+      }
+      if (e.request.method === 'GET'&& e.request.url.includes('/logout')) {
+        // Exclude handling redirects for POST requests
+        return;
+      }
+      if (e.request.method === 'POST'&& e.request.url.includes('/delete')) {
+        // Exclude handling redirects for POST requests
+        return;
+      }
     console.log('[ServiceWorker] Fetch', e.request.url);
     e.respondWith((async () => {
         const cacheResponse = await caches.match(e.request);
@@ -44,25 +67,52 @@ self.addEventListener('fetch', (e) => {
         try {
             const fetchResponse = await fetch(e.request);
             if (fetchResponse.ok) {
-                // Clone the response since it can only be consumed once
-                const clonedResponse = fetchResponse.clone();
-                const cache = await caches.open(cacheName);
-                console.log('[ServiceWorker] Caching new resource:', e.request.url);
-                cache.put(e.request, clonedResponse);
-                return fetchResponse;
+              // Clone the response since it can only be consumed once
+              const clonedResponse = fetchResponse.clone();
+              const cache = await caches.open(cacheName);
+              console.log('[ServiceWorker] Caching new resource:', e.request.url);
+              cache.put(e.request, clonedResponse);
+              return fetchResponse;
             }
-        } catch (error) {
+          } catch (error) {
             console.log('[ServiceWorker] Fetch failed; returning offline page instead.', error);
-        }
-
+          }
         // If the request is for an image in the "/static/uploads" directory,
         // serve a fallback image or offline image
         const requestUrl = new URL(e.request.url);
         if (requestUrl.pathname.startsWith('/static/uploads')) {
             return caches.match('/static/fallback-image.jpg');
         }
+        if (requestUrl.pathname.startsWith('/upload') || requestUrl.pathname.startsWith('/gallery') || requestUrl.pathname.startsWith('/index')) {
+            // Serve the request from the static cache
+            const staticCacheResponse = await caches.match(e.request, { cacheName: staticcache });
+            if (staticCacheResponse) {
+                console.log('[ServiceWorker] Static Cache hit:', e.request.url);
+                return staticCacheResponse;
+            }
+        }
 
         // For other requests, return an offline page
         return caches.match('/offline.html');
     })());
+});
+self.addEventListener('message', (e) => {
+    if (e.data.action === 'clearCache') {
+        caches.keys().then((cacheNames) => {
+          cacheNames.forEach((cacheName) => {
+            if (cacheName.startsWith('flask-PWA-v1')) {
+              if (cacheName.startsWith('flask-PWA-v1/static/uploads') || cacheName === 'flask-PWA-v1/gallery-html') {
+                caches.delete(cacheName);
+                console.log('[ServiceWorker] Deleted cache:', cacheName);
+              }
+            }
+          });
+        });
+        e.waitUntil((async () => {
+            const cache = await caches.open(cacheName);
+            console.log('[ServiceWorker] recaching');
+            await cache.addAll(filesToCache);
+        })());
+    }
+    
 });
